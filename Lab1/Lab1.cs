4ac83084d92ef20c6a043f30aee12022490acbe3
Косарев Виталий <omelistum@vk.com>
2022-02-15 13:39:59 +0300

    изменил код

--- Lab1/Lab1.cs
+++ Lab1/Lab1.cs
@@ -7,8 +7,12 @@ namespace Lab1
     {
         public static void Main(string[] args)
         {
+            Console.Write("Введите необходимое количество элементов в массиве: ");
             int a = int.Parse(Console.ReadLine());
             a *= Convert.ToInt32(Math.Pow(3, 2));
+            a /= 9;
+            a++;
+            a--;
             int[] massiv = new int[a];
             Random rnd = new Random();
             for (int i = 0; i < a; i++)
